88709976e654cb851a0e9465f89c640e27f7b660
Your Name <you@example.com>
2021-03-20 18:51:56 +0100

    added wget to core software since its gone from prior scripts

--- installation/100-install-core-software-v1.sh
+++ installation/100-install-core-software-v1.sh
@@ -12,6 +12,7 @@ set -e
 ##################################################################################################################
 
 #software from 'normal' repositories
+sudo pacman -S --noconfirm --needed wget
 sudo pacman -S --noconfirm --needed archey3 baobab bleachbit catfish clementine conky curl
 sudo pacman -S --noconfirm --needed darktable dconf-editor
 sudo pacman -S --noconfirm --needed dmidecode 
