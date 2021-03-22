859ebd029c7da773589fcef985bf6dd198b5dc38
Your Name <you@example.com>
2021-03-22 08:00:06 +0100

    Added essential core software selection

---
+++ installation/101-install-essential-core-software.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# Written to be used on 64 bits computers
+# Author 	: 	Erik Dubois
+# Website 	: 	http://www.erikdubois.be
+##################################################################################################################
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+#software from 'normal' repositories
+sudo pacman -S --noconfirm --needed wget
+sudo pacman -S --noconfirm --needed archey3 baobab curl
+sudo pacman -S --noconfirm --needed dconf-editor
+sudo pacman -S --noconfirm --needed dmidecode 
+sudo pacman -S --noconfirm --needed evince chromium
+sudo pacman -S --noconfirm --needed galculator git gksu glances gnome-disk-utility 
+sudo pacman -S --noconfirm --needed gnome-font-viewer gnome-screenshot gnome-system-monitor gnome-terminal gnome-tweak-tool 
+sudo pacman -S --noconfirm --needed gparted gpick grsync
+sudo pacman -S --noconfirm --needed hardinfo hddtemp htop 
+sudo pacman -S --noconfirm --needed inxi lm_sensors lsb-release meld mlocate mpv
+sudo pacman -S --noconfirm --needed nemo net-tools numlockx polkit-gnome 
+sudo pacman -S --noconfirm --needed ristretto screenfetch
+sudo pacman -S --noconfirm --needed simplescreenrecorder 
+sudo pacman -S --noconfirm --needed terminator
+sudo pacman -S --noconfirm --needed vlc vnstat wget unclutter  
+
+sudo systemctl enable vnstat
+sudo systemctl start vnstat
+
+###############################################################################################
+
+# installation of zippers and unzippers
+sudo pacman -S --noconfirm --needed unace unrar zip unzip sharutils  uudeview  arj cabextract file-roller
+
+###############################################################################################
+
+
+echo "################################################################"
+echo "###################    core software installed  ################"
+echo "################################################################"
+
